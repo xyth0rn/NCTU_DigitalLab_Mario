4f458092b914bf963d1c09d2e37d26eb79a94caa
llenN <167954410+llenN1028@users.noreply.github.com>
2024-05-04 19:06:44 +0800

    spring.v

--- game_calc/spring.v
+++ game_calc/spring.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2024/05/03 23:57:58
-// Design Name: 
-// Module Name: spring
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module spring( input sys_clk,
                input [8:0] char_X,
