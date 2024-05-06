b010833dec37298117efbda3e1e62e5068bcc20a
Yander-code <66255028+Yander-code@users.noreply.github.com>
2024-05-07 01:04:27 +0800

    unfinished

--- game_calc/char.v
+++ game_calc/char.v
@@ -1,30 +1,47 @@
 module char(
 	input sys_clk,
-	input [8:0] blk_map_X, //the size is wrong, should be 480
-	input [8:0] blk_map_Y,
-	input [3:0] update_pos_scroll, //up down left right
-	input jump, fall,
-	output reg [8:0] char_X,
-	output reg [8:0] char_Y
+	//input [9:0] blk_map_X, //the size is wrong, should be 480
+	//input [9:0] blk_map_Y,
+	input [3:0] mov, //from keypad //u d l r
+	//input jump, fall,
+	output reg [9:0] char_X,
+	output reg [9:0] char_Y
 );
+reg [9:0] blk_map_X=10'd0;
+reg [9:0] blk_map_Y=10'd0;
 
 initial begin 
-	char_X=9'd0;
-	char_Y=9'd0;
+	char_X=10'd143;
+	char_Y=10'd34;
+end
+
+reg [29:0] counter=30'd0;
+reg clk_2hz=1'd0;
+always @(posedge sys_clk) begin
+    if(counter==30'd4999999)begin
+        counter<=30'd0;
+        clk_2hz<=~clk_2hz;
+    end
+    else begin
+        counter<=counter+30'd1;
+    end
 end
 
 always @(posedge sys_clk) begin
-	if(update_pos_scroll[3]==1'b1 && ~(blk_map_X[char_X] & blk_map_Y[char_Y+9'b1])) begin
-		char_X<=char_X; char_Y<=char_Y+9'b1;
+    if(char_X==10'd640) begin
+        char_X=10'd0;
+    end
+	else if(mov[3]==1'b1 && ~(blk_map_X[char_X] & blk_map_Y[char_Y+10'b1])) begin
+		char_X<=char_X; char_Y<=char_Y+10'b1;
 	end
-	if(update_pos_scroll[2]==1'b1 && ~(blk_map_X[char_X] & blk_map_Y[char_Y-9'b1])) begin
-		char_X<=char_X; char_Y<=char_Y-9'b1;
+	else if(mov[2]==1'b1 && ~(blk_map_X[char_X] & blk_map_Y[char_Y-10'b1])) begin
+		char_X<=char_X; char_Y<=char_Y-10'b1;
 	end
-	if(update_pos_scroll[1]==1'b1 && ~(blk_map_X[char_X-9'b1] & blk_map_Y[char_Y])) begin
-		char_X<=char_X-9'b1; char_Y<=char_Y;
+	else if(mov[1]==1'b1 && ~(blk_map_X[char_X-10'b1] & blk_map_Y[char_Y])) begin
+		char_X<=char_X-10'b1; char_Y<=char_Y;
 	end
-	if(update_pos_scroll[0]==1'b1&& ~(blk_map_X[char_X+9'b1] & blk_map_Y[char_Y])) begin
-		char_X<=char_X+9'b1; char_Y<=char_Y;
+	else if(mov[0]==1'b1&& ~(blk_map_X[char_X+10'b1] & blk_map_Y[char_Y])) begin
+		char_X<=char_X+10'b1; char_Y<=char_Y;
 	end
 end
 
