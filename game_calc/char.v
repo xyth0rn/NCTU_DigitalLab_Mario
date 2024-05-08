759b9ccdf7ec267de45630cae84d864672edbbc0
Yander-code <66255028+Yander-code@users.noreply.github.com>
2024-05-08 18:57:22 +0800

    Add files via upload

--- game_calc/char.v
+++ game_calc/char.v
@@ -1,9 +1,7 @@
 module char(
 	input sys_clk,
-	//input [9:0] blk_map_X, //the size is wrong, should be 480
-	//input [9:0] blk_map_Y,
 	input [3:0] mov, //from keypad //u d l r
-	//input jump, fall,
+	
 	output reg [9:0] char_X,
 	output reg [9:0] char_Y
 );
@@ -11,14 +9,14 @@ reg [9:0] blk_map_X=10'd0;
 reg [9:0] blk_map_Y=10'd0;
 
 initial begin 
-	char_X=10'd143;
-	char_Y=10'd34;
+	char_X=10'd1;
+	char_Y=10'd0;
 end
 
 reg [29:0] counter=30'd0;
 reg clk_2hz=1'd0;
 always @(posedge sys_clk) begin
-    if(counter==30'd4999999)begin
+    if(counter==30'd499999)begin
         counter<=30'd0;
         clk_2hz<=~clk_2hz;
     end
@@ -27,21 +25,18 @@ always @(posedge sys_clk) begin
     end
 end
 
-always @(posedge sys_clk) begin
-    if(char_X==10'd640) begin
-        char_X=10'd0;
+always @(posedge clk_2hz) begin
+    if(char_X==10'd0) begin
+        //char_X<=char_X; //cannot move anymore
     end
-	else if(mov[3]==1'b1 && ~(blk_map_X[char_X] & blk_map_Y[char_Y+10'b1])) begin
-		char_X<=char_X; char_Y<=char_Y+10'b1;
-	end
-	else if(mov[2]==1'b1 && ~(blk_map_X[char_X] & blk_map_Y[char_Y-10'b1])) begin
-		char_X<=char_X; char_Y<=char_Y-10'b1;
-	end
 	else if(mov[1]==1'b1 && ~(blk_map_X[char_X-10'b1] & blk_map_Y[char_Y])) begin
-		char_X<=char_X-10'b1; char_Y<=char_Y;
+		char_X<=char_X-10'd1; char_Y<=char_Y;
+	end
+	if(char_X>=10'd480) begin
+	   //char_X<=char_X;
 	end
 	else if(mov[0]==1'b1&& ~(blk_map_X[char_X+10'b1] & blk_map_Y[char_Y])) begin
-		char_X<=char_X+10'b1; char_Y<=char_Y;
+		char_X<=char_X+10'd1; char_Y<=char_Y;
 	end
 end
 
--- game_calc/scroll.v
+++ game_calc/scroll.v
@@ -3,44 +3,21 @@ module scroll(
 	input [9:0] char_X, 
 	input [9:0] char_Y,
 	//output reg [3:0] update_pos_scroll,
-	output reg [9:0] bg_pos=10'd0 
+	output reg [9:0] bg_pos 
 );
 
-reg [29:0] counter=30'd0;
-reg clk_2hz=1'd0;
-always @(posedge sys_clk) begin
-    if(counter==30'd4999999)begin
-        counter<=30'd0;
-        clk_2hz<=~clk_2hz;
-    end
-    else begin
-        counter<=counter+30'd1;
-    end
+initial begin
+    bg_pos=10'd0;
 end
 
-always @(posedge clk_2hz) begin
-	if(bg_pos==10'd640) begin 
-		bg_pos<=10'd0;
-	end
-	else if(char_X<=bg_pos+10'd90) begin
-		bg_pos<=bg_pos-10'd1;
-	end
-	else if(char_X>=bg_pos+10'd270) begin
+always @(posedge sys_clk) begin
+	if(bg_pos<10'd480 && char_X>=bg_pos+10'd270) begin
 		bg_pos<=bg_pos+10'd1;
 	end
+	if(bg_pos>10'd0 && char_X<=bg_pos+10'd90) begin
+		bg_pos<=bg_pos-10'd1;
+	end
+	
 end
-
-/*reg [29:0] counter=30'd0;
-always @(posedge sys_clk) begin
-    if(counter>=30'd9999999)begin
-        counter<=30'd0;
-        bg_pos<=bg_pos+10'd10;
-    end
-    else begin
-        counter<=counter+30'd1;
-    end
-
-end*/
-
 
 endmodule
