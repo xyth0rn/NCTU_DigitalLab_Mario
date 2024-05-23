3be6df77b3b1eacc6fe3887f601465fc934b0840
llenN <167954410+llenN1028@users.noreply.github.com>
2024-05-23 16:38:29 +0800

    Add files via upload

---
+++ game_calc/objects/star1.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+
+module STAR1(
+	input sys_clk,
+	input  [9:0] char_X,
+	input  [9:0] char_Y,
+	input  [9:0] bg_pos,
+	input RST_N,
+    output wire [9:0] star1_x,
+    output wire [9:0] star1_y,
+    output touch_star1,
+    output  en
+);
+  reg [9:0] star1_x_r = 10'd236;
+  reg [9:0] star1_y_r = 10'd200;
+  reg enable = 1'b1;
+  reg touch;
+  assign star1_x = star1_x_r - bg_pos;
+  assign star1_y = star1_y_r;
+  assign touch_star1 = touch;
+  assign en = enable;
+  
+  always@(posedge sys_clk or negedge RST_N)
+    begin
+      if(!RST_N)
+        begin
+          enable <= 1'b1;
+          touch <= 1'b0;
+        end
+      else if( ((((char_X >= star1_x_r) && (char_X <= star1_x_r + 10'd12)) || ((char_X + 10'd12 >= star1_x_r) && (char_X + 10'd12 <= star1_x_r + 10'd12)))) & ((((char_Y >= star1_y_r) && (char_Y <= star1_y_r + 10'd12)) || ((char_Y + 10'd12 >= star1_y_r) && (char_Y + 10'd12 <= star1_y_r + 10'd12)))) ) 
+        begin 
+          enable <= 1'b0;
+          touch <= 1'b1;
+        end
+      else
+        touch <= touch;
+    end
+endmodule
+
+
---
+++ game_calc/objects/star3.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+
+module STAR3(input sys_clk,
+	input  [9:0] char_X,
+	input  [9:0] char_Y,
+	input  [9:0] bg_pos,
+	input RST_N,
+    output wire [9:0] star3_x,
+    output wire [9:0] star3_y,
+    output touch_star3,
+    output  en
+);
+  reg [9:0] star3_x_r = 10'd347;
+  reg [9:0] star3_y_r = 10'd56;
+  reg enable = 1'b1;
+  reg touch;
+  assign star3_x = star3_x_r - bg_pos;
+  assign star3_y = star3_y_r;
+  assign touch_star3 = touch;
+  assign en = enable;
+  
+  always@(posedge sys_clk or negedge RST_N)
+    begin
+      if(!RST_N)
+        begin
+          enable <= 1'b1;
+          touch <= 1'b0;
+        end
+      else if( ((((char_X >= star3_x_r) && (char_X <= star3_x_r + 10'd12)) || ((char_X + 10'd12 >= star3_x_r) && (char_X + 10'd12 <= star3_x_r + 10'd12)))) & ((((char_Y >= star3_y_r) && (char_Y <= star3_y_r + 10'd12)) || ((char_Y + 10'd12 >= star3_y_r) && (char_Y + 10'd12 <= star3_y_r + 10'd12)))) ) 
+        begin 
+          enable <= 1'b0;
+          touch <= 1'b1;
+        end
+      else
+        touch <= touch;
+    end
+    
+endmodule
